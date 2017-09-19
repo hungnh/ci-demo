e80e455361d80fda4a67a8d5f58f481e042baa55
hungnh <hungnh.uet@gmail.com>
2017-09-19 23:51:10 +0700

    refactor + optimize imports...

--- src/main/java/uet/hungnh/ci/CIDemoApplication.java
+++ src/main/java/uet/hungnh/ci/CIDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CIDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CIDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CIDemoApplication.class, args);
+    }
 }
