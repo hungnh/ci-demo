c6832ec187e1c6d550e35d58e1449b1c7518e0c6
hungnh <hungnh.uet@gmail.com>
2017-09-19 23:38:11 +0700

    Initialize project

---
+++ src/main/java/uet/hungnh/ci/CIDemoApplication.java
@@ -0,0 +1,12 @@
+package uet.hungnh.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CIDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CIDemoApplication.class, args);
+	}
+}
